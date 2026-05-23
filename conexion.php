146afe982d0f34faa2041e39356be87bb85582b5
OscielMaca <osielmaca@gmail.com>
2026-05-23 18:27:48 -0500

    Connect online database

--- conexion.php
+++ conexion.php
@@ -1,7 +1,16 @@
 <?php
-$conexion = pg_connect("host=localhost dbname=notas user=postgres password=1234");
+
+$conexion = pg_connect("
+host=dpg-d893cip9rddc738uiikg-a
+port=5432
+dbname=notas_wntx
+user=notas_wntx_user
+password=0A3VJqWw7PCcu6ptDY8U2m3a9aFN17El
+sslmode=require
+");
 
 if (!$conexion) {
     die("Error de conexión a PostgreSQL");
 }
+
 ?>
