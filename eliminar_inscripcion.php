49b66cb022305d27e9479e35608554693dbeb91f
OscielMaca <osielmaca@gmail.com>
2026-05-23 19:42:43 -0500

    Fix conexion variable

--- eliminar_inscripcion.php
+++ eliminar_inscripcion.php
@@ -6,7 +6,7 @@ if(isset($_POST['cod_est'], $_POST['cod_cur'], $_POST['year'], $_POST['periodo']
     $cod_est = (int)$_POST['cod_est'];
     $cod_cur = (int)$_POST['cod_cur'];
     $year = (int)$_POST['year'];
-    $periodo = pg_escape_string(conexion,$_POST['periodo']);
+    $periodo = pg_escape_string($conexion,$_POST['periodo']);
 
     $query = "
     DELETE FROM inscripciones
